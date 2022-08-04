b656aab71fe49a55f48fe3ff6dad4545bd017670
Rohan Nelson <rohan.nelson@gmail.com>
2022-08-04 14:35:43 +0800

    Implemented Intersection Observer

--- header.js
+++ header.js
@@ -1,13 +1,26 @@
 const headernav = document.querySelector("header");
 const main = document.querySelector("main");
-window.addEventListener("scroll", () => responsiveHeader(), { passive: true });
-function responsiveHeader() {
-    if (window.scrollY > 10) {
-        headernav.classList.add("scrolled");
-        main.classList.add("scrolled");
-    }
-    else {
-        headernav.classList?.remove("scrolled");
-        main.classList?.remove("scrolled");
-    }
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0
+};
+
+const obsCallback = (entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (!entry.isIntersecting) {
+            headernav.classList.add("scrolled");
+        }
+        else {
+            headernav.classList?.remove("scrolled");
+        };
+    });
 }
+
+headerObserver = new IntersectionObserver(obsCallback, options);
+headerObserver.observe(document.querySelector('#header-trigger'));
+
+//The target must be a descendant of the root element - for some reason the header doesn't seem to count as the descendant of body
+//You can use a placeholder element at the top of the page to detect when you're at the top or not.
